c395006b14917819ee89b412cf6719c3b99e0215
Max Lindblom <max.g.lindblom@gmail.com>
2023-04-05 11:51:56 +0200

    content styling

--- src/routes/Config.tsx
+++ src/routes/Config.tsx
@@ -8,6 +8,8 @@ interface ApiKeyState {
   lastFour: string;
 }
 
+let message: string;
+
 export function Config() {
   const [isActive, setIsActive] = useState(false);
   const [apiKeyState, setApiKeyState] = useState<ApiKeyState>({
@@ -15,9 +17,9 @@ export function Config() {
     lastFour: "",
   });
   const [inputValue, setInputValue] = useState("");
+  const isValidInput = inputValue.length > 4;
 
   const onClickTest = function () {
-    setIsActive(true);
     const apiKey = getSavedApiKey();
     if (apiKey) {
       const lastFour = apiKey.slice(-4);
@@ -25,53 +27,72 @@ export function Config() {
     } else {
       setApiKeyState({ exists: false, lastFour: "" });
     }
+    message = apiKeyState.exists
+      ? `There is an API key saved! The last 4 characters are ${apiKeyState.lastFour}`
+      : "No API key saved";
+    setIsActive(true);
   };
 
   const setNewModel = function (model: string) {
+    message =
+      model === "gpt-4"
+        ? "Cutting edge, boss! Now using gpt-4 model"
+        : `Now using ${model} model`;
+    setIsActive(true);
     setModel(model);
   };
 
   const onClickSet = function () {
-    if (inputValue.length > 4) {
+    if (isValidInput) {
       setApiKey(inputValue);
+      message = `Set openAiApiKey to ${inputValue}`;
+      setIsActive(true);
       setInputValue("");
     }
   };
 
-  const message = apiKeyState.exists
-    ? `There is an API key saved! The last 4 characters are ${apiKeyState.lastFour}`
-    : "No API key saved";
-
   return (
     <div className="centered">
+      <PopUp
+        isActive={isActive}
+        setInactive={() => setIsActive(false)}
+        message={message}
+      ></PopUp>
       <div className="content-wrapper column-layout">
         <h1>Config</h1>
-        <p>
-          On this page you can set up your API key to get yourself ready to use
-          GPTony
-        </p>
-        <p>
-          The API key is stored in local storage. Check if there is an API key
-          saved:
-        </p>
-        <button className="button test-api-key" onClick={onClickTest}>
-          Test API key
-        </button>
-        <PopUp
-          isActive={isActive}
-          setInactive={() => setIsActive(false)}
-          message={message}
-        ></PopUp>
-        <p>
-          If there is no API key saved, you can enter one here. Generate one
-          from the{" "}
-          <a href="https://platform.openai.com/account/api-keys" target="blank">
-            OpenAi Website
-          </a>
-          , and keep in mind you will need to have billing enabled.
-          Alternatively, you can add it manually to local storage. The key is{" "}
-          <code>{API_STORAGE_KEY}</code>
-        </p>
+        <div>
+          <p>
+            On this page you can set up your API key to get yourself ready to
+            use GPTony
+          </p>
+        </div>
+        <div>
+          <p>
+            The API key is stored in local storage. Check if there is an API key
+            saved:
+          </p>
+        </div>
+        <div>
+          <button className="button test-api-key" onClick={onClickTest}>
+            Test API key
+          </button>
+        </div>
+        <div>
+          <p>
+            If there is no API key saved, you can enter one here. Generate one
+            from the{" "}
+            <a
+              href="https://platform.openai.com/account/api-keys"
+              target="blank"
+            >
+              OpenAi Website
+            </a>
+            , and keep in mind you will need to have billing enabled.
+            Alternatively, you can add it manually to local storage. The key is{" "}
+            <code>{API_STORAGE_KEY}</code>
+          </p>
+        </div>
+
         <div className="row-layout">
           <input
             value={inputValue}
@@ -79,20 +100,30 @@ export function Config() {
             type="text"
             onChange={(e) => setInputValue(e.target.value)}
           ></input>
-          <button className="button" type="button" onClick={onClickSet}>
+          <button
+            className="button"
+            type="button"
+            onClick={onClickSet}
+            disabled={!isValidInput}
+          >
             Set
           </button>
         </div>
-        <p>
-          GPTony uses the gpt-4 model by default. If you don't have access to
-          it, you can use the earlier gpt-3.5-turbo model instead:
-        </p>
+        <div>
+          <p>
+            GPTony uses the gpt-4 model by default. If you don't have access to
+            it, you can use the earlier gpt-3.5-turbo model instead:
+          </p>
+        </div>
         <div className="row-layout">
-          <button className="button" onClick={() => setNewModel("gpt-4")}>
+          <button
+            className="button model-button"
+            onClick={() => setNewModel("gpt-4")}
+          >
             Use gpt-4
           </button>
           <button
-            className="button"
+            className="button model-button"
             onClick={() => setNewModel("gpt-3.5-turbo")}
           >
             Use gpt-3.5-turbo
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,43 +5,59 @@ export function Home() {
     <div className="centered">
       <div className="content-wrapper column-layout">
         <h1>Hey how you doin'? Welcome to GPTony.</h1>
-        <p>
-          This is a web-based wrapper for chatGPT, using the{" "}
-          <a href="https://platform.openai.com/docs/guides/chat" target="blank">
-            openai chat completions API
-          </a>
-          . It behaves mostly the same way as the regular chatGPT chat but with
-          some fun alterations, all packaged within a beautiful brutalist UI.
-        </p>
-        <p>
-          To use GPTony, you will need a working API key from openai, that has
-          billing enabled.
-        </p>
-        <p className="bold">
-          NOTE: GPTony will perform requests that charge your account.
-        </p>
-        <p>
-          The amount billed will be up to you and your usage. GPTony does not
-          perform superflous requests. Read more about pricing, billing, and
-          setting up your account{" "}
-          <a
-            href="https://platform.openai.com/account/billing/overview"
-            target="blank"
-          >
-            on openai's website
-          </a>
-          .
-        </p>
-        <p>
-          In the chat window, you have the option to make Tony only respond with
-          code. This is experimental, and what it does is append a prompt to the
-          end of the query to only respond with code. Results may vary.
-        </p>
-        <p>
-          The first thing to do would be to set up your API key. Read more about
-          that on <Link to="/config">the config page</Link>. If that's already
-          set up, jump right in to <Link to="/chat">the chat</Link>!
-        </p>
+        <div>
+          <p>
+            This is a web-based wrapper for chatGPT, using the{" "}
+            <a
+              href="https://platform.openai.com/docs/guides/chat"
+              target="blank"
+            >
+              openai chat completions API
+            </a>
+            . It behaves mostly the same way as the regular chatGPT chat but
+            with some fun alterations, all packaged within a beautiful brutalist
+            UI.
+          </p>
+        </div>
+        <div>
+          <p>
+            To use GPTony, you will need a working API key from openai, that has
+            billing enabled.
+          </p>
+        </div>
+        <div>
+          <p className="bold">
+            NOTE: GPTony will perform requests that charge your account.
+          </p>
+        </div>
+        <div>
+          <p>
+            The amount billed will be up to you and your usage. GPTony does not
+            perform superflous requests. Read more about pricing, billing, and
+            setting up your account{" "}
+            <a
+              href="https://platform.openai.com/account/billing/overview"
+              target="blank"
+            >
+              on openai's website
+            </a>
+            .
+          </p>
+        </div>
+        <div>
+          <p>
+            In the chat window, you have the option to make Tony only respond
+            with code. This is experimental, and what it does is append a prompt
+            to the end of the query to only respond with code. Results may vary.
+          </p>
+        </div>
+        <div>
+          <p>
+            The first thing to do would be to set up your API key. Read more
+            about that on <Link to="/config">the config page</Link>. If that's
+            already set up, jump right in to <Link to="/chat">the chat</Link>!
+          </p>
+        </div>
       </div>
     </div>
   );
