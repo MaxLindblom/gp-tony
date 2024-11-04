4fcc5f0000d0348e243115af573daa090d23284b
Max Lindblom <max.g.lindblom@gmail.com>
2024-11-04 08:22:26 +0100

    Expand loading string options

--- src/components/LoadingIndicator.tsx
+++ src/components/LoadingIndicator.tsx
@@ -1,15 +1,51 @@
+import { useEffect, useState } from "react";
+
 interface LoadingIndicatorProps {
   isLoading: boolean;
 }
 
+const LOADING_STRINGS = [
+  "Pondering",
+  "Thinking",
+  "Calculating",
+  "Generating",
+  "Computing",
+  "Simulating",
+  "Analyzing",
+  "Evaluating",
+  "Processing",
+  "Optimizing",
+  "Compiling",
+  "Decoding",
+  "Encoding",
+  "Decrypting",
+  "Encrypting",
+  "Validating",
+  "Verifying",
+  "Authenticating",
+];
+
+const randomizeLoadingString = () =>
+  LOADING_STRINGS[Math.floor(Math.random() * LOADING_STRINGS.length)];
+
 export function LoadingIndicator({ isLoading }: LoadingIndicatorProps) {
+  const [loadingString, setLoadingString] = useState(randomizeLoadingString());
+
+  useEffect(() => {
+    const loadingInterval = setInterval(
+      () => setLoadingString(randomizeLoadingString()),
+      2000
+    );
+    return () => clearInterval(loadingInterval);
+  }, []);
+
   if (!isLoading) {
     return null;
   }
 
   return (
     <div style={{ padding: "1rem" }}>
-      <div className="loading">Pondering</div>
+      <div className="loading">{loadingString}</div>
     </div>
   );
 }
